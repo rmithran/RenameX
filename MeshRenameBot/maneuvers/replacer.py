f223f3883c9f069800d2274e0c5fc46a6e5f6f47
JD <97616567+Kajee2002@users.noreply.github.com>
2022-08-17 12:27:00 +0530

    fr

--- MeshRenameBot/maneuvers/replacer.py
+++ MeshRenameBot/maneuvers/replacer.py
@@ -1,11 +1,10 @@
 import os
 from os import environ
 
-RE1TXT = os.environ.get("RE1", "@Username1")
-RE2TXT = os.environ.get("RE2", "@Username1")
-RE3TXT = os.environ.get("RE3", "⚠️ Uploaded By @HQFilms4U")
-RE4TXT = os.environ.get("RE4", "@Username3")
-RE5TXT = os.environ.get("RE5", "@Username4")
-RE6TXT = os.environ.get("RE6", "@Username5")
+RE1 = os.environ.get("RE1", "@Username1")
+RE2 = os.environ.get("RE2", "@Username1")
+RE3 = os.environ.get("RE3", "⚠️ Uploaded By @HQFilms4U")
+RE4 = os.environ.get("RE4", "@Username3")
+RE5 = os.environ.get("RE5", "@Username4")
 
 REPLACED = os.environ.get("REPLACED", "@Hollywood_0980")
