52744406fdb8ba68cf2b0c102947b93692d77873
JD <97616567+Kajee2002@users.noreply.github.com>
2022-08-17 11:03:16 +0530

    pullx

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, ""]
-        API_ID = [int, 0]
-        BOT_TOKEN = [str, ""]
+        DATABASE_URL = [str, "postgres://ahaoudqqzjftpg:f1cded7940937479eddcec5a0eda389a4380aba6415cf3fe84c8d8bd55389b86@ec2-52-30-92-209.eu-west-1.compute.amazonaws.com:5432/dbrdljsvd4a9s0"]
+        API_HASH = [str, "8b0473750d327598c8585b0049f09e2d"]
+        API_ID = [int, 6309305]
+        BOT_TOKEN = [str, "1962266386:AAFRatdcvflwfotaXDVIz-YcvaQzu3VVFDQ"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
