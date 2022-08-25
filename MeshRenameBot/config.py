663e5b0ca4e572048c2a259c83d88784e8cf70d6
rmithran <rmithran0311@gmail.com>
2022-08-25 16:21:38 +0530

    dune

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -7,7 +7,7 @@ except ImportError:
         DATABASE_URL = [str, "postgres://ahaoudqqzjftpg:f1cded7940937479eddcec5a0eda389a4380aba6415cf3fe84c8d8bd55389b86@ec2-52-30-92-209.eu-west-1.compute.amazonaws.com:5432/dbrdljsvd4a9s0"]
         API_HASH = [str, "8b0473750d327598c8585b0049f09e2d"]
         API_ID = [int, 6309305]
-        BOT_TOKEN = [str, "1962266386:AAFRatdcvflwfotaXDVIz-YcvaQzu3VVFDQ"]
+        BOT_TOKEN = [str, "5654495482:AAFh4_ItyTDi8gRvtFd5URp3T5jIWSABL9Y"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
